401ec1f5bd6f15e0d1f529bcb4768e8824f61548
Your Name <gchkumar999@gmail.com>
2025-03-05 19:55:51 +0530

    txt

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,2 +1,18 @@
 let userName="vrama"
 console.log(userName)
+
+
+//Explicit and Implicit
+
+//Implicit
+let captain="dhoni"
+
+captain="Rahul"
+
+captain="Virat"
+
+//Explicit
+
+let name:string ="Rahul";
+let phone:number=45;
+let male:boolean=true;
